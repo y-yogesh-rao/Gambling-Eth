91e4d7fe694ab390f8739c71467272de0e7be366
Yogesh Rao <yogesh.rao@contract.planetdds.com>
2022-12-03 19:36:37 +0530

    Added enter function using global require function check

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -3,8 +3,14 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Lottery {
     address public manager;
+    address[] public players;
 
     constructor() {
         manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > 0.001 ether);
+        players.push(msg.sender);
     }
 }
